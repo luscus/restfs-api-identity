5f069679895939222432db40fa07dfd7c59f9c11
luscus <luscus.redbeard@gmail.com>
2016-04-28 21:44:52 +0200

    added support for api initialisation

--- RESTfs/api/ApiConfig.js
+++ RESTfs/api/ApiConfig.js
@@ -1,10 +1,21 @@
 'use strict';
 
+var config = require('../../lib/config');
+var merge  = require('merge');
+
 module.exports = {
     name: 'identity',
     alias: 'id',
     description: 'Allows the CRUD operations for the user account and the authentication',
     roles: {
         IDENTITY_MANAGER: 'can create/delete users'
+    },
+    init: function init (options) {
+
+        // save options in config module
+        merge.recursive(config, options);
+
+        // init database connection
+        require('../../lib/elastic-client')();
     }
 };
