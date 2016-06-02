56a01b7058a4a306e2255fe45d19c99a24734114
luscus <luscus.redbeard@gmail.com>
2016-06-02 23:34:03 +0200

    added token lifetime checks and unit tests

--- RESTfs/modules/parseAuthenticationHeader.js
+++ RESTfs/modules/parseAuthenticationHeader.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const jwt        = require('../../lib/jwt');
+const constants  = require('../../lib/constants');
 
 module.exports = {
   chainPosition: 'pre',
@@ -8,9 +9,30 @@ module.exports = {
   handler: function parseAuthenticationHeader(request, context) {
 
       if (context.meta.headers.raw.authentication) {
-          var tokenInfo = jwt.parse(context.meta.headers.raw.authentication, context);
+          const tokenInfo = jwt.parse(context);
+          const timestamp = Math.floor(Date.now() / 1000);
+          const lifespan  = context.config.token && context.config.token.lifespan || constants.token.lifespan;
 
-          context.user  = tokenInfo.payload.user;
+
+          if (!tokenInfo.claim.iat) {
+              // token is not valid
+              context.logger.debug('    => invalid JWT token');
+              return;
+          } else if (tokenInfo.claim.nbf && timestamp < tokenInfo.claim.nbf) {
+              // token is not valid yet
+              context.logger.debug('    => JWT token will be valid at:\n', tokenInfo.claim.nbf);
+              return;
+          } else if (tokenInfo.claim.exp && timestamp >= tokenInfo.claim.exp) {
+              // token has expired
+              context.logger.debug('    => JWT token has expired at:\n', tokenInfo.claim.exp);
+              return;
+          } else if (tokenInfo.claim.iat && timestamp > tokenInfo.claim.iat + lifespan) {
+              // token is not valid yet
+              context.logger.debug('    => JWT token has expired at:\n', tokenInfo.claim.iat + lifespan);
+              return;
+          }
+
+          context.user  = tokenInfo.claim.user;
           context.logger.debug('    => Authenticated User:\n', context.user);
       }
   }
---
+++ test/modules/parseAuthenticationHeader.spec.js
@@ -0,0 +1,59 @@
+'use strict';
+
+require('chai').should();
+
+const expect      = require('chai').expect;
+const clone       = require('clone');
+const apiModule   = require('../../RESTfs/modules/parseAuthenticationHeader');
+const jwt         = require('../../lib/jwt');
+const reqContext  = require('../testContext');
+
+const secret      = 'my-big-secret';
+
+var testContext   = null;
+var testTimestamp = null;
+
+describe('[' + __filename.substring(__filename.indexOf('/test/') + 1) + '] - parseAuthenticationHeader ', function() {
+
+    beforeEach(function () {
+        testTimestamp        = Math.floor(Date.now() / 1000);
+        testContext          = clone(reqContext);
+
+        delete testContext.user;
+
+        // add headers
+        testContext.meta.headers = {
+            raw: {}
+        };
+    });
+
+    it('should do nothing if no JWT token exists', function() {
+        apiModule.handler({}, testContext);
+        expect(testContext.user).to.equal(undefined);
+    });
+
+    it('should ignore pending JWT tokens', function() {
+        testContext.meta.headers.raw.authentication = 'JWT ' + jwt.tokenize(reqContext, secret, 10000, testTimestamp);
+
+        apiModule.handler({}, testContext);
+
+        expect(testContext.user).to.equal(undefined);
+    });
+
+    it('should ignore expired JWT tokens', function() {
+        testContext.meta.headers.raw.authentication = 'JWT ' + jwt.tokenize(reqContext, secret, testTimestamp - 10000);
+
+        apiModule.handler({}, testContext);
+
+        expect(testContext.user).to.equal(undefined);
+    });
+
+    it('should retrieve user from JWT token', function() {
+        testContext.meta.headers.raw.authentication = 'JWT ' + jwt.tokenize(reqContext, secret, testTimestamp - 100);
+
+        apiModule.handler({}, testContext);
+
+        expect(testContext.user).to.deep.equal(reqContext.user);
+    });
+
+});
